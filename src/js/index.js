75e6113977fce1bff302d1bc70e2e91dc1dbec59
Davi-D18 <luiz.araujo@alunomicrolins.com.br>
2024-12-03 09:19:17 -0300

    Ajustes no javascript

--- src/js/index.js
+++ src/js/index.js
@@ -1,17 +1,38 @@
-function handleIntersection(entries, observer) {
-  entries.forEach((entry) => {
-    if (entry.isIntersecting) {
-      entry.target.classList.remove("hide");
-      entry.target.classList.add("show");
-    } else {
-      entry.target.classList.remove("show");
-      entry.target.classList.add("hide");
-    }
-  });
+function handleIntersection(entries) {
+  const isMobile = window.matchMedia("(max-width: 768px)").matches;
+
+  if (isMobile) {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.remove("hide");
+        entry.target.classList.add("show");
+      } else {
+        entry.target.classList.remove("show");
+        entry.target.classList.add("hide");
+      }
+    });
+  } else {
+    let count = 0;
+
+    const timer = setInterval(() => {
+      if (count >= entries.length) {
+        clearInterval(timer);
+      }
+
+      const entry = entries[count];
+
+      if (entry.isIntersecting) {
+        entry.target.classList.remove("hide");
+        entry.target.classList.add("show");
+      }
+
+      count++;
+    }, 550);
+  }
 }
 
 const observer = new IntersectionObserver(handleIntersection, {
-  threshold: 0.18,
+  threshold: 0.19,
 });
 
 document.querySelectorAll(".card").forEach((card) => {
